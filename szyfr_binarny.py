8c0d82b3a13204ea8c7a18cdeeddf0e2b16a96af
dawnik <dawnik@dawnik>
2019-08-02 17:51:34 +0200

    program na wejściu pobiera ilosć testów następnie ciąg binarny i przetwarza go podając wynik np. 00000 = A, itd

--- szyfr_binarny.py
+++ szyfr_binarny.py
@@ -1,40 +1,43 @@
 slownik = {
-            "00000": 'A',
-            "00001": 'B',
-            "00010": 'C',
-            "00011": 'D',
-            "00100": 'E',
-            "00101": 'F',
-            "00110": 'G',
-            "00111": 'H',
-            "01000": 'I',
-            "01001": 'J',
-            "01010": 'K',
-            "01011": 'L',
-            "01100": 'M',
-            "01101": 'N',
-            "01110": 'O',
-            "01111": 'P',
-            "10000": 'Q',
-            "10001": 'R',
-            "10010": 'S',
-            "10011": 'T',
-            "10100": 'U',
-            "10101": 'V',
-            "10110": 'W',
-            "10111": 'X',
-            "11000": 'Y',
-            "11001": 'Z',
+    "00000": 'A',
+    "00001": 'B',
+    "00010": 'C',
+    "00011": 'D',
+    "00100": 'E',
+    "00101": 'F',
+    "00110": 'G',
+    "00111": 'H',
+    "01000": 'I',
+    "01001": 'J',
+    "01010": 'K',
+    "01011": 'L',
+    "01100": 'M',
+    "01101": 'N',
+    "01110": 'O',
+    "01111": 'P',
+    "10000": 'Q',
+    "10001": 'R',
+    "10010": 'S',
+    "10011": 'T',
+    "10100": 'U',
+    "10101": 'V',
+    "10110": 'W',
+    "10111": 'X',
+    "11000": 'Y',
+    "11001": 'Z',
 }
 petli = int(input("Ile testów: "))
-slowo = input("Haslo: ")
 
-haslo= ''
-word = ''
-licznik = 0
-kolejka = 1
+warunek = petli
+iterator_warunek = 1
 
-while kolejka <= petli:
+while iterator_warunek <= warunek:
+    slowo = input("Haslo: ")
+    haslo= ''
+    word = ''
+    licznik = 0
+    kolejka = 1
+
     for litera in slowo:
         haslo += litera
         licznik += 1
@@ -45,5 +48,5 @@ while kolejka <= petli:
                 licznik = 0
                 kolejka += 1
                 break
-
-print(word)
+    iterator_warunek += 1
+    print(word)
